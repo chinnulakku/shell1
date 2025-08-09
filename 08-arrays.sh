67504caa90e9d05bbf1e426f41de39cd1da26781
chinnulakku4 <chinnulakku4@gmail.com>
2025-08-09 16:22:27 +0530

    shell1 file

---
+++ 08-arrays.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FRUITS=("Mango" "Orange" "Apple" "Pineapple")
+
+echo "first value: ${FRUITS[0]}"
+
+echo "second value: ${FRUITS[1]}"
+
+echo "third value: ${FRUITS[2]}"
+
+echo "fourth value: ${FRUITS[3]}"
+
+echo "All value: ${FRUITS[@]}"

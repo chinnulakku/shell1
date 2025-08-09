c25eccb468adb910c0375c4391b0c748fd155e8d
chinnulakku4 <chinnulakku4@gmail.com>
2025-08-09 16:36:50 +0530

    shell1 file

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,10 +2,10 @@
 
 NUBMER=$1
 
-if[ $NUBMER -gt 100]
+if[ $NUBMER -gt 100 ] 
 then
 
     echo "Given number is $NUBMER is greater than 100"
 else
-    echo "given number is $NUBMER is not greater than 100"
+    echo "Given number is $NUBMER is not greater than 100"
 fi

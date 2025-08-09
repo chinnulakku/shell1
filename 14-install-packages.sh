701898ef692e478ddd7aed3ae178383730af5684
chinnulakku4 <chinnulakku4@gmail.com>
2025-08-09 23:50:14 +0530

    shell1 file

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -7,6 +7,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+}
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run theis script with root access"
@@ -15,4 +21,16 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+# git mysql postfix net-tools
+# package=git for first time
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+        VALIDATE $? "Installation of $package"
+done
+

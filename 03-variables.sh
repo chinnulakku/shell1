21985d62f0041a221fc0f733b6d913aab60d145e
chinnulakku4 <chinnulakku4@gmail.com>
2025-08-09 15:00:27 +0530

    shell1 file

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=CHINNU
+PERSON2=LAKKU
+
+echo "$PERSON1:hello $PERSON2, how are you"
+echo "$PERSON2:hii $PERSON1, good thank you"
+echo "$PERSON1:Thank You $PERSON2"
+echo "$PERSON2: Welcome $PERSON1"

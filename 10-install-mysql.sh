90a8d1ea8618be488dafda400c8f53421039bf41
chinnulakku4 <chinnulakku4@gmail.com>
2025-08-09 17:22:35 +0530

    shell1 file

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -12,10 +12,20 @@ fi
 
 yum install mysql -y
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then 
     echo "ERROR:: Installing MYSQL is failed"
     exit 1
 else
     echo "Installing MYSQL is SUCCESS"
+fi
+
+yum install git -y
+
+if [ $? -ne 0]
+then
+    echo  "ERROR: Installing Git is failed"
+    EXIT 1
+else
+    echo "Installing Git is SUCCESS"
 fi

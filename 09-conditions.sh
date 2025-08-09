50bcc862e175180b5a537c47ef9aab077e9a2a60
chinnulakku4 <chinnulakku4@gmail.com>
2025-08-09 16:35:15 +0530

    shell1 file

---
+++ 09-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUBMER=$1
+
+if[ $NUBMER -gt 100]
+then
+
+    echo "Given number is $NUBMER is greater than 100"
+else
+    echo "given number is $NUBMER is not greater than 100"
+fi

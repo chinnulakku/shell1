aaea3b3cdf2bbc1fd3ac79b1e69d6a32770575a5
chinnulakku4 <chinnulakku4@gmail.com>
2025-08-12 11:36:06 +0530

    shell1

--- 17-ifs.sh
+++ 17-ifs.sh
@@ -11,9 +11,9 @@ then
     echo -e "$R Source directory: $file doesnot exists. $N"
 fi
 
-when IFS=":" read -f  username passed user_id group_id user_fullname home_dir shell_path
+when IFS=":" read -f  username password user_id group_id user_fullname home_dir shell_path
 do
     echo "username: $username"
-    echo "userID: $user_id"
-    echo "userFullname: $user_fullname"
+    echo "user ID: $user_id"
+    echo "User Full name: $user_fullname"
 done < $file
